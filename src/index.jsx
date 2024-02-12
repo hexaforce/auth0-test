113bd1eea5e8077a9ca2d38177239b9791514d00
tantaka <tomokazu.tantaka@gmail.com>
2024-02-12 22:43:21 +0900

    update

--- src/index.jsx
+++ src/index.jsx
@@ -5,23 +5,22 @@ import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 import history from "./utils/history";
 
-const onRedirectCallback = (appState) => {
-  history.push(appState && appState.returnTo ? appState.returnTo : window.location.pathname);
-};
-
-const providerConfig = {
-  domain: process.env.REACT_APP_DOMAIN,
-  clientId: process.env.REACT_APP_CLIENT_ID,
-  onRedirectCallback,
-  authorizationParams: {
-    redirect_uri: window.location.origin,
-    audience: process.env.REACT_APP_AUDIENCE
-  },
-};
-
 const root = createRoot(document.getElementById("root"));
+
 root.render(
-  <Auth0Provider {...providerConfig}>
+  <Auth0Provider
+    domain={process.env.REACT_APP_DOMAIN}
+    clientId={process.env.REACT_APP_CLIENT_ID}
+    onRedirectCallback={(appState, user) => {
+      console.log("appState:", appState);
+      console.log("user:", user);
+      history.push(appState && appState.returnTo ? appState.returnTo : window.location.pathname);
+    }}
+    authorizationParams={{
+      audience: process.env.REACT_APP_AUDIENCE,
+      redirect_uri: window.location.origin,
+    }}
+  >
     <App />
   </Auth0Provider>
 );
