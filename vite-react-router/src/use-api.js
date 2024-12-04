c4d859209d9494b5e59c496ff603c30d19cc8382
Tomokazu Tantaka <t.tantaka@eukarya.io>
2024-12-04 19:43:32 +0900

    aaa

--- vite-react-router/src/use-api.js
+++ vite-react-router/src/use-api.js
@@ -3,38 +3,16 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 export const useApi = (url, options) => {
   const { getAccessTokenSilently } = useAuth0();
-  const [state, setState] = useState({
-    error: null,
-    loading: true,
-    data: null,
-  });
-
+  const [state, setState] = useState({ error: null, loading: true, data: null });
   useEffect(() => {
     (async () => {
       try {
         const { audience, scope, ...fetchOptions } = options;
-        const accessToken = await getAccessTokenSilently({
-          authorizationParams: { audience, scope },
-        });
-        const res = await fetch(url, {
-          ...fetchOptions,
-          headers: {
-            ...fetchOptions.headers,
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
-        setState({
-          ...state,
-          data: await res.json(),
-          error: null,
-          loading: false,
-        });
+        const accessToken = await getAccessTokenSilently({ authorizationParams: { audience, scope } });
+        const res = await fetch(url, { ...fetchOptions, headers: { ...fetchOptions.headers, Authorization: `Bearer ${accessToken}` } });
+        setState({ ...state, data: await res.json(), error: null, loading: false });
       } catch (error) {
-        setState({
-          ...state,
-          error,
-          loading: false,
-        });
+        setState({ ...state, error, loading: false });
       }
     })();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
